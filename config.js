a832fe890eff737ace1fea2d192a5a8adc4ea86b
Dapabot <101037424+Dapabot@users.noreply.github.com>
2022-03-25 22:53:22 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 let fs = require('fs')
 
-global.owner = ['+48729759866'] // Put your number here
+global.owner = ['48729759866'] // Put your number here
 global.mods = [] // Want some help?
 global.prems = JSON.parse(fs.readFileSync('./src/premium.json'))
 global.APIs = { // API Prefix
